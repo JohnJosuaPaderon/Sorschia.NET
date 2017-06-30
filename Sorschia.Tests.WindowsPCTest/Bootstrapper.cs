90f39962c0c67c39d93973dc76febac747ccaccd
johnpad53 <johnjosua.paderon@outlook.com>
2017-07-01 06:26:07 +0800

    Added: App.config; App.xaml; App.xaml.cs; Bootstrapper.cs; logo.ico; packages.config; Sorschia.Tests.WindowsPCTest.csproj

---
+++ Sorschia.Tests.WindowsPCTest/Bootstrapper.cs
@@ -0,0 +1,18 @@
+﻿using Prism.Unity;
+using Sorschia.Tests.Views;
+using Sorschia.ViewModels;
+
+namespace Sorschia.Tests
+{
+    class Bootstrapper : WindowsPcBootstrapper
+    {
+        protected override void ConfigureContainer()
+        {
+            base.ConfigureContainer();
+
+            Container.RegisterTypeForNavigation<SampleView>();
+
+            MainWindowViewModel.InitialContent = nameof(SampleView);
+        }
+    }
+}
