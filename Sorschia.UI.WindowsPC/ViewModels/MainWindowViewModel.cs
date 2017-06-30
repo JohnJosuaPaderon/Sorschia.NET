5d652565c351716a1a4f30339ffd7e1f518fbd27
johnpad53 <johnjosua.paderon@outlook.com>
2017-07-01 06:29:14 +0800

    Added: MainWindowViewModel.cs

---
+++ Sorschia.UI.WindowsPC/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Prism.Events;
+using Prism.Mvvm;
+using Prism.Regions;
+
+namespace Sorschia.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        public const string CONTENT_REGION = "ContentRegion";
+        public static string InitialContent { get; set; }
+
+        public MainWindowViewModel(IRegionManager regionManager, IEventAggregator eventAggregator)
+        {
+            RegionManager = regionManager;
+            EventAggregator = eventAggregator;
+        }
+
+        private readonly IRegionManager RegionManager;
+        private readonly IEventAggregator EventAggregator;
+
+        public void Load()
+        {
+            RegionManager.RequestNavigate(CONTENT_REGION, InitialContent);
+        }
+    }
+}
