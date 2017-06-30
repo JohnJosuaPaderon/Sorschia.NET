ae3c2c6b52fa3ae51b93e6b9699617bf6a1482bd
johnpad53 <johnjosua.paderon@outlook.com>
2017-07-01 06:29:01 +0800

    Added: MaterialDesignPrimaryColor.cs

---
+++ Sorschia.UI.WindowsPC/Themes/MaterialDesignPrimaryColor.cs
@@ -0,0 +1,25 @@
+﻿namespace Sorschia.Themes
+{
+    public enum MaterialDesignPrimaryColor
+    {
+        Yellow,
+        Amber,
+        DeepOrange,
+        LightBlue,
+        Teal,
+        Cyan,
+        Pink,
+        Green,
+        DeepPurple,
+        Indigo,
+        LightGreen,
+        Blue,
+        Lime,
+        Red,
+        Orange,
+        Purple,
+        BlueGrey,
+        Grey,
+        Brown
+    }
+}
