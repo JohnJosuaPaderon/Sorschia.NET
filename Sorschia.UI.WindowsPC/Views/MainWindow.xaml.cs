c90de928ab29cb499e100d14677107ed0fff6f92
johnpad53 <johnjosua.paderon@outlook.com>
2017-07-01 06:29:31 +0800

    Added: MainWindow.xaml; MainWindow.xaml.cs

---
+++ Sorschia.UI.WindowsPC/Views/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using MahApps.Metro.Controls;
+using Sorschia.ViewModels;
+
+namespace Sorschia.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += (s, e) => ViewModel.Load();
+        }
+
+        public MainWindowViewModel ViewModel => DataContext as MainWindowViewModel;
+    }
+}
