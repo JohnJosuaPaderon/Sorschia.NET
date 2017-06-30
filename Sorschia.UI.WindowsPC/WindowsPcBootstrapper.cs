2d3461faa38ff2a9cb9cad63ba4b880674c4c0d1
johnpad53 <johnjosua.paderon@outlook.com>
2017-07-01 06:30:06 +0800

    Added: app.config; logo.ico; packages.config; Sorschia.UI.WindowsPC.csproj; WindowsPCBootstrapper.cs

---
+++ Sorschia.UI.WindowsPC/WindowsPcBootstrapper.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using Prism.Unity;
+using Sorschia.Views;
+
+namespace Sorschia
+{
+    public class WindowsPcBootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.TryResolve<MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+    }
+}
