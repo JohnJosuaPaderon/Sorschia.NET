3a3ac2eabeee8d6e7557754ac9b349d04ceec164
johnpad53 <johnjosua.paderon@outlook.com>
2017-07-18 08:19:48 +0800

    Added: IServiceCollectionExtension.cs;

---
+++ Sorschia.SqlServer/Extensions/IServiceCollectionExtension.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Sorschia.Data.Rdbms;
+
+namespace Sorschia.Extensions
+{
+    public static class IServiceCollectionExtension
+    {
+        public static IServiceCollection UseSqlServer(this IServiceCollection services)
+        {
+            services.AddSingleton<ISqlConnectionEstablisher, SqlConnectionEstablisher>();
+            services.AddSingleton<ISqlHelper, SqlHelper>();
+
+            return services;
+        }
+    }
+}
