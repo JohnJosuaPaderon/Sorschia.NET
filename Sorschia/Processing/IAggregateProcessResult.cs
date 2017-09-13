8ee07b05fa581120c46885c28d889c1e0d0ae6cf
johnpad53 <johnjosua.paderon@outlook.com>
2017-09-13 16:30:48 +0800

    Added: IAggregateProcessResult.cs; ProcessResult.cs;

---
+++ Sorschia/Processing/IAggregateProcessResult.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sorschia.Processing
+{
+    public interface IAggregateProcessResult : IEnumerable<IProcessResult>
+    {
+
+    }
+}
---
+++ Sorschia/Processing/ProcessResult.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Sorschia.Processing
+{
+    public class ProcessResult : IProcessResult
+    {
+        public static ProcessResult Failed(string message)
+        {
+            return new ProcessResult(ProcessResultStatus.Failed, message);
+        }
+
+        public static ProcessResult Failed(Exception exception)
+        {
+            return new ProcessResult(ProcessResultStatus.Failed, "An exception has been thrown.", exception);
+        }
+
+        public static ProcessResult Failed(string message, Exception exception)
+        {
+            return new ProcessResult(ProcessResultStatus.Failed, message, exception);
+        }
+
+        public static ProcessResult Success()
+        {
+            return new ProcessResult(ProcessResultStatus.Success, "Process successfully performed.");
+        }
+
+        public static ProcessResult Success(string message)
+        {
+            return new ProcessResult(ProcessResultStatus.Success, message);
+        }
+
+        public ProcessResult(ProcessResultStatus status, string message, Exception exception = null)
+        {
+            Status = status;
+            Message = message;
+            Exception = exception;
+        }
+
+        public ProcessResultStatus Status { get; }
+        public string Message { get; }
+        public Exception Exception { get; }
+    }
+}
