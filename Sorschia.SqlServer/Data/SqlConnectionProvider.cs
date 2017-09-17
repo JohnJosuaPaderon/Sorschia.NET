36880ca59589f7f3c0cf5b82e3542212206b1862
johnpad53 <johnjosua.paderon@outlook.com>
2017-09-18 00:54:17 +0800

    Added: SqlCommandProvider.cs; SqlConnectionProvider.cs; SqlTransactionProvider.cs;

---
+++ Sorschia.SqlServer/Data/SqlConnectionProvider.cs
@@ -0,0 +1,76 @@
+﻿using Sorschia.Configurations;
+using System;
+using System.Data.SqlClient;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Sorschia.Data
+{
+    public sealed class SqlConnectionProvider : IDbConnectionProvider<SqlConnection>
+    {
+        public SqlConnectionProvider(IConnectionStringSource connectionStringSource)
+        {
+            _ConnectionStringSource = connectionStringSource ?? throw new SorschiaException(SorschiaExceptionType.ValueRequired);
+        }
+
+        private readonly IConnectionStringSource _ConnectionStringSource;
+
+        private SqlConnection Instantiate(string connectionStringKey)
+        {
+            if (string.IsNullOrWhiteSpace(connectionStringKey))
+            {
+                throw new SorschiaException(SorschiaExceptionType.KeyRequired);
+            }
+
+            return new SqlConnection(_ConnectionStringSource[connectionStringKey]);
+        }
+
+        public SqlConnection Establish(string connectionStringKey)
+        {
+            var connection = Instantiate(connectionStringKey);
+
+            try
+            {
+                connection.Open();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return connection;
+        }
+
+        public async Task<SqlConnection> EstablishAsync(string connectionStringKey)
+        {
+            var connection = Instantiate(connectionStringKey);
+
+            try
+            {
+                await connection.OpenAsync();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return connection;
+        }
+
+        public async Task<SqlConnection> EstablishAsync(string connectionStringKey, CancellationToken cancellationToken)
+        {
+            var connection = Instantiate(connectionStringKey);
+
+            try
+            {
+                await connection.OpenAsync(cancellationToken);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return connection;
+        }
+    }
+}
---
+++ Sorschia.SqlServer/Data/SqlTransactionProvider.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.SqlClient;
+
+namespace Sorschia.Data
+{
+    public sealed class SqlTransactionProvider : IDbTransactionProvider<SqlConnection, SqlTransaction>
+    {
+        public SqlTransaction Initialize(SqlConnection connection)
+        {
+            return connection.BeginTransaction();
+        }
+    }
+}
