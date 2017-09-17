246696756811503774d074fa2d2d80db579ce852
johnpad53 <johnjosua.paderon@outlook.com>
2017-09-18 00:54:35 +0800

    Added: SqlParameterCollectionExtension.cs; SqlServerIntegrator.cs;

---
+++ Sorschia.SqlServer/Extensions/SqlParameterCollectionExtension.cs
@@ -0,0 +1,33 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace Sorschia.Extensions
+{
+    public static class SqlParameterCollectionExtension
+    {
+        private static SqlParameter Instantiate(string name, object value, ParameterDirection direction)
+        {
+            return new SqlParameter()
+            {
+                ParameterName = name,
+                Value = value,
+                Direction = direction
+            };
+        }
+
+        public static void AddIn(this SqlParameterCollection instance, string name, object value)
+        {
+            instance.Add(Instantiate(name, value, ParameterDirection.Input));
+        }
+
+        public static void AddOut(this SqlParameterCollection instance, string name)
+        {
+            instance.Add(Instantiate(name, null, ParameterDirection.Output));
+        }
+
+        public static void AddInOut(this SqlParameterCollection instance, string name, object value)
+        {
+            instance.Add(Instantiate(name, value, ParameterDirection.InputOutput));
+        }
+    }
+}
---
+++ Sorschia.SqlServer/Extensions/SqlServerIntegrator.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Sorschia.Data;
+using System.Data.SqlClient;
+
+namespace Sorschia.Extensions
+{
+    public static class SqlServerIntegrator
+    {
+        public static IServiceCollection AddSqlServer(this IServiceCollection instance)
+        {
+            instance.AddSingleton<IDbHelper<SqlConnection, SqlTransaction, SqlCommand, IQueryParameter>, DbHelper<SqlConnection, SqlTransaction, SqlCommand, IQueryParameter>>();
+            instance.AddSingleton<IDbConnectionProvider<SqlConnection>, SqlConnectionProvider>();
+            instance.AddSingleton<IDbTransactionProvider<SqlConnection, SqlTransaction>, SqlTransactionProvider>();
+            instance.AddSingleton<IDbCommandProvider<SqlConnection, SqlTransaction, SqlCommand>, SqlCommandProvider>();
+
+            return instance;
+        }
+    }
+}
