1223799ec046d41743d70b4138b11508cf453f81
johnpad53 <johnjosua.paderon@outlook.com>
2017-09-18 00:50:40 +0800

    Added: NameBase.cs;

---
+++ Sorschia.Application/Entities/NameBase.cs
@@ -0,0 +1,129 @@
+﻿using Sorschia.Utilities;
+
+namespace Sorschia.Entities
+{
+    public abstract class NameBase
+    {
+        static NameBase()
+        {
+            _AcronymBuilder = SorschiaApp.Resolve<IAcronymBuilder>() ?? throw new SorschiaException(nameof(IAcronymBuilder), SorschiaExceptionType.ServiceRequired);
+            _FullNameBuilder = SorschiaApp.Resolve<IFullNameBuilder>() ?? throw new SorschiaException(nameof(IFullNameBuilder), SorschiaExceptionType.ServiceRequired);
+            _InformalFullNameBuilder = SorschiaApp.Resolve<IInformalFullNameBuilder>() ?? throw new SorschiaException(nameof(IInformalFullNameBuilder), SorschiaExceptionType.ServiceRequired);
+        }
+
+        private static readonly IAcronymBuilder _AcronymBuilder;
+        private static readonly IFullNameBuilder _FullNameBuilder;
+        private static readonly IInformalFullNameBuilder _InformalFullNameBuilder;
+
+        private bool FullNameRefreshRequired;
+        private bool InformalFullNameRefreshRequired;
+        private bool MiddleInitialsRefreshRequired;
+
+        private string _FirstName;
+        private string _MiddleName;
+        private string _LastName;
+        private string _NameExtension;
+        private string _FullName;
+        private string _InformalFullName;
+        private string _MiddleInitials;
+
+        public string FirstName
+        {
+            get { return _FirstName; }
+            set
+            {
+                if (_FirstName != value)
+                {
+                    _FirstName = value;
+                    FullNameRefreshRequired = true;
+                    InformalFullNameRefreshRequired = true;
+                }
+            }
+        }
+
+        public string MiddleName
+        {
+            get { return _MiddleName; }
+            set
+            {
+                if (_MiddleName != value)
+                {
+                    _MiddleName = value;
+                    FullNameRefreshRequired = true;
+                    InformalFullNameRefreshRequired = true;
+                    MiddleInitialsRefreshRequired = true;
+                }
+            }
+        }
+
+        public string LastName
+        {
+            get { return _LastName; }
+            set
+            {
+                if (_LastName != value)
+                {
+                    _LastName = value;
+                    FullNameRefreshRequired = true;
+                    InformalFullNameRefreshRequired = true;
+                }
+            }
+        }
+
+        public string NameExtension
+        {
+            get { return _NameExtension; }
+            set
+            {
+                if (_NameExtension != value)
+                {
+                    _NameExtension = value;
+                    FullNameRefreshRequired = true;
+                    InformalFullNameRefreshRequired = true;
+                }
+            }
+        }
+
+        public string FullName
+        {
+            get
+            {
+                if (FullNameRefreshRequired)
+                {
+                    _FullName = _FullNameBuilder.Build(LastName, FirstName, NameExtension, MiddleName);
+                    FullNameRefreshRequired = false;
+                }
+
+                return _FullName;
+            }
+        }
+
+        public string InformalFullName
+        {
+            get
+            {
+                if (InformalFullNameRefreshRequired)
+                {
+                    _InformalFullName = _InformalFullNameBuilder.Build(FirstName, MiddleInitials, LastName, NameExtension);
+                    InformalFullNameRefreshRequired = false;
+                }
+
+                return _InformalFullName;
+            }
+        }
+
+        public string MiddleInitials
+        {
+            get
+            {
+                if (MiddleInitialsRefreshRequired)
+                {
+                    _MiddleInitials = _AcronymBuilder.Build(MiddleName);
+                    MiddleInitialsRefreshRequired = false;
+                }
+
+                return _MiddleInitials;
+            }
+        }
+    }
+}
