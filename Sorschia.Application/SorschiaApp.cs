e08970a96e9fa3690d5fefdfa1465eb9c6f03d98
johnpad53 <johnjosua.paderon@outlook.com>
2017-09-18 00:51:16 +0800

    Added: logo.ico; Sorschia.Application.csproj; SorschiaApp.cs; SorschiaBootstrapper.cs;

---
+++ Sorschia.Application/SorschiaApp.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Sorschia.Utilities;
+using System;
+
+namespace Sorschia
+{
+    public sealed class SorschiaApp
+    {
+        static SorschiaApp()
+        {
+            Current = new SorschiaApp();
+        }
+
+        public static SorschiaApp Current { get; }
+
+        public static T Resolve<T>()
+        {
+            return Current.ServiceProvider.GetService<T>();
+        }
+
+        public static void BuildApp(SorschiaBootstrapper bootstrapper)
+        {
+            var services = new ServiceCollection();
+            bootstrapper.ConfigureServices(services);
+
+            if (!string.IsNullOrWhiteSpace(bootstrapper.BaseDirectory))
+            {
+                DirectoryResolver.Resolve(bootstrapper.BaseDirectory);
+            }
+
+            Current.ServiceProvider = services.BuildServiceProvider();
+            Current.BaseDirectory = bootstrapper.BaseDirectory;
+        }
+
+        public IServiceProvider ServiceProvider { get; private set; }
+        public string BaseDirectory { get; private set; }
+    }
+}
---
+++ Sorschia.Application/SorschiaBootstrapper.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Sorschia
+{
+    public abstract class SorschiaBootstrapper
+    {
+        internal protected virtual void ConfigureServices(IServiceCollection services)
+        {
+            // TODO: Default services.
+        }
+
+        internal protected string BaseDirectory { get; set; }
+    }
+}
