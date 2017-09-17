96f39f905369af1ba3d6441cdf69d816a1a42a29
johnpad53 <johnjosua.paderon@outlook.com>
2017-09-18 00:48:53 +0800

    Added: EnumerableProcessResult.cs; Updated: ProcessResult.cs; Removed: IAggregateProcessResult.cs;

---
+++ Sorschia/Processing/EnumerableProcessResult.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sorschia.Processing
+{
+    public class EnumerableProcessResult<T> : IEnumerableProcessResult<T>
+    {
+        public static EnumerableProcessResult<T> Failed(string message)
+        {
+            return new EnumerableProcessResult<T>(null, ProcessResultStatus.Failed, message);
+        }
+
+        public static EnumerableProcessResult<T> Failed(Exception exception)
+        {
+            return new EnumerableProcessResult<T>(null, ProcessResultStatus.Failed, "An exception has been thrown.", exception);
+        }
+
+        public static EnumerableProcessResult<T> Failed(string message, Exception exception)
+        {
+            return new EnumerableProcessResult<T>(null, ProcessResultStatus.Failed, message, exception);
+        }
+
+        public static EnumerableProcessResult<T> Success(IEnumerable<T> dataList)
+        {
+            return new EnumerableProcessResult<T>(dataList, ProcessResultStatus.Success, "Process successfully completed.");
+        }
+
+        public static EnumerableProcessResult<T> Success(IEnumerable<T> dataList, string message)
+        {
+            return new EnumerableProcessResult<T>(dataList, ProcessResultStatus.Success, message);
+        }
+
+        public EnumerableProcessResult(IEnumerable<T> dataList, ProcessResultStatus status, string message, Exception exception = null)
+        {
+            DataList = dataList;
+            Status = status;
+            Message = message;
+            Exception = exception;
+        }
+
+        public IEnumerable<T> DataList { get; }
+        public ProcessResultStatus Status { get; }
+        public string Message { get; }
+        public Exception Exception { get; }
+    }
+}
--- Sorschia/Processing/ProcessResult.cs
+++ Sorschia/Processing/ProcessResult.cs
@@ -21,7 +21,7 @@ namespace Sorschia.Processing
 
         public static ProcessResult Success()
         {
-            return new ProcessResult(ProcessResultStatus.Success, "Process successfully performed.");
+            return new ProcessResult(ProcessResultStatus.Success, "Process successfully completed.");
         }
 
         public static ProcessResult Success(string message)
@@ -39,5 +39,46 @@ namespace Sorschia.Processing
         public ProcessResultStatus Status { get; }
         public string Message { get; }
         public Exception Exception { get; }
+    }
+
+    public class ProcessResult<T> : IProcessResult<T>
+    {
+        public static ProcessResult<T> Failed(string message)
+        {
+            return new ProcessResult<T>(default(T), ProcessResultStatus.Failed, message);
+        }
+
+        public static ProcessResult<T> Failed(Exception exception)
+        {
+            return new ProcessResult<T>(default(T), ProcessResultStatus.Failed, "An exception has been thrown.", exception);
+        }
+
+        public static ProcessResult<T> Failed(string message, Exception exception)
+        {
+            return new ProcessResult<T>(default(T), ProcessResultStatus.Failed, message, exception);
+        }
+
+        public static ProcessResult<T> Success(T data)
+        {
+            return new ProcessResult<T>(data, ProcessResultStatus.Success, "Process successfully completed.");
+        }
+
+        public static ProcessResult<T> Success(T data, string message)
+        {
+            return new ProcessResult<T>(data, ProcessResultStatus.Success, message);
+        }
+
+        public ProcessResult(T data, ProcessResultStatus status, string message, Exception exception = null)
+        {
+            Status = status;
+            Message = message;
+            Exception = exception;
+            Data = data;
+        }
+
+        public ProcessResultStatus Status { get; }
+        public string Message { get; }
+        public Exception Exception { get; }
+        public T Data { get; }
     }
 }
