4ff8781f72ea4eb1b1d1df9296a963f7f701f0be
johnpad53 <johnjosua.paderon@outlook.com>
2017-09-20 21:41:07 +0800

    Added: App.config; App.xaml; App.xaml.cs; Bootstrapper.cs; logo.ico; packages.config; Sorschia.DevTeamDesktop.csproj;

---
+++ Sorschia.DevTeam.Desktop/Bootstrapper.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+using Prism.Unity;
+using Sorschia.DevTeam.Views;
+
+namespace Sorschia.DevTeam
+{
+    class Bootstrapper : UnityBootstrapper
+    {
+        static Bootstrapper()
+        {
+            Current = new Bootstrapper();
+        }
+
+        public static Bootstrapper Current { get; }
+
+        public static void RunBootstrapper()
+        {
+            Current.Run();
+        }
+
+        private Bootstrapper()
+        {
+
+        }
+
+        protected override DependencyObject CreateShell()
+        {
+            return Container.TryResolve<MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+
+        protected override void ConfigureContainer()
+        {
+            base.ConfigureContainer();
+        }
+    }
+}
