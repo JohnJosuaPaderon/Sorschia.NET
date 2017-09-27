bc4ba21bfc19e1f6dcd2d8dac7a824c258e60df2
johnpad53 <johnjosua.paderon@outlook.com>
2017-09-27 16:10:44 +0800

    Added: SqlServerIntegrator.cs;

---
+++ Sorschia.DailyTask.SqlServer/Extensions/SqlServerIntegrator.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Sorschia.DailyTask.EntityConverters;
+using Sorschia.DailyTask.EntityProcesses;
+using Sorschia.Extensions;
+
+namespace Sorschia.DailyTask.Extensions
+{
+    public static class SqlServerIntegrator
+    {
+        public static IServiceCollection UseSqlServerForDailyTask(this IServiceCollection instance)
+        {
+            instance.AddSqlServer();
+
+            instance.AddTransient<IDTaskConverter, DTaskConverter>();
+            instance.AddTransient<IDeleteDTask, DeleteDTask>();
+            instance.AddTransient<IGetDTaskById, GetDTaskById>();
+            instance.AddTransient<IGetDTaskList, GetDTaskList>();
+            instance.AddTransient<IInsertDTask, InsertDTask>();
+            instance.AddTransient<IUpdateDTaskStatus, UpdateDTaskStatus>();
+
+            return instance;
+        }
+    }
+}
