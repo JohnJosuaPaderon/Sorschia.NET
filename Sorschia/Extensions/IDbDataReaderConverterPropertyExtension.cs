ce089f5047773d5966f245bd10a56a09683e9772
johnpad53 <johnjosua.paderon@outlook.com>
2017-09-27 16:07:58 +0800

    Added: IDbDataReaderConverterPropertyExtension.cs;

---
+++ Sorschia/Extensions/IDbDataReaderConverterPropertyExtension.cs
@@ -0,0 +1,13 @@
+﻿using Sorschia.Data;
+using System;
+
+namespace Sorschia.Extensions
+{
+    public static class IDbDataReaderConverterPropertyExtension
+    {
+        public static T TryGetValue<T>(this IDbDataReaderConverterProperty<T> instance, Func<string, T> expression, string arg)
+        {
+            return instance.TryGetValue(expression(arg));
+        }
+    }
+}
