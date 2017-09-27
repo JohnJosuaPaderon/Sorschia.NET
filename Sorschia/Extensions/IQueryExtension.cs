db58d55a15382bf1cb31f9e450e683aa33e230a3
johnpad53 <johnjosua.paderon@outlook.com>
2017-09-27 10:31:11 +0800

    Added: IQueryExtension.cs;

---
+++ Sorschia/Extensions/IQueryExtension.cs
@@ -0,0 +1,68 @@
+﻿using Sorschia.Data;
+using System.Data.Common;
+
+namespace Sorschia.Extensions
+{
+    public static class IQueryExtension
+    {
+        public static IQuery<IQueryParameter> AddInParameter(this IQuery<IQueryParameter> instance, string name, object value)
+        {
+            instance.Parameters.AddIn(name, value);
+            return instance;
+        }
+
+        public static IQuery<IQueryParameter> AddOutParameter(this IQuery<IQueryParameter> instance, string name)
+        {
+            instance.Parameters.AddOut(name);
+            return instance;
+        }
+
+        public static IQuery<IQueryParameter> AddInOutParameter(this IQuery<IQueryParameter> instance, string name, object value)
+        {
+            instance.Parameters.AddInOut(name, value);
+            return instance;
+        }
+
+        public static IQuery<TCommand, IQueryParameter> AddInParameter<TCommand>(this IQuery<TCommand, IQueryParameter> instance, string name, object value)
+            where TCommand : DbCommand
+        {
+            instance.Parameters.AddIn(name, value);
+            return instance;
+        }
+
+        public static IQuery<TCommand, IQueryParameter> AddOutParameter<TCommand>(this IQuery<TCommand, IQueryParameter> instance, string name)
+            where TCommand : DbCommand
+        {
+            instance.Parameters.AddOut(name);
+            return instance;
+        }
+
+        public static IQuery<TCommand, IQueryParameter> AddInOutParameter<TCommand>(this IQuery<TCommand, IQueryParameter> instance, string name, object value)
+            where TCommand : DbCommand
+        {
+            instance.Parameters.AddInOut(name, value);
+            return instance;
+        }
+
+        public static IQuery<TData, TCommand, IQueryParameter> AddInParameter<TData, TCommand>(this IQuery<TData, TCommand, IQueryParameter> instance, string name, object value)
+           where TCommand : DbCommand
+        {
+            instance.Parameters.AddIn(name, value);
+            return instance;
+        }
+
+        public static IQuery<TData, TCommand, IQueryParameter> AddOutParameter<TData, TCommand>(this IQuery<TData, TCommand, IQueryParameter> instance, string name)
+            where TCommand : DbCommand
+        {
+            instance.Parameters.AddOut(name);
+            return instance;
+        }
+
+        public static IQuery<TData, TCommand, IQueryParameter> AddInOutParameter<TData, TCommand>(this IQuery<TData, TCommand, IQueryParameter> instance, string name, object value)
+            where TCommand : DbCommand
+        {
+            instance.Parameters.AddInOut(name, value);
+            return instance;
+        }
+    }
+}
