019ec9b8e3ab0902add07b034c0c11db4c38d436
johnpad53 <johnjosua.paderon@outlook.com>
2017-09-27 16:08:58 +0800

    Added: IDTaskManager.cs;

---
+++ Sorschia.DailyTask/EntityManagers/IDTaskManager.cs
@@ -0,0 +1,26 @@
+﻿using Sorschia.DailyTask.Entities;
+using Sorschia.Processing;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Sorschia.DailyTask.EntityManagers
+{
+    public interface IDTaskManager
+    {
+        IProcessResult<IDTask> Delete(IDTask dTask);
+        Task<IProcessResult<IDTask>> DeleteAsync(IDTask dTask);
+        Task<IProcessResult<IDTask>> DeleteAsync(IDTask dTask, CancellationToken cancellationToken);
+        IProcessResult<IDTask> GetById(long dTaskId);
+        Task<IProcessResult<IDTask>> GetByIdAsync(long dTaskId);
+        Task<IProcessResult<IDTask>> GetByIdAsync(long dTaskId, CancellationToken cancellationToken);
+        IEnumerableProcessResult<IDTask> GetList();
+        Task<IEnumerableProcessResult<IDTask>> GetListAsync();
+        Task<IEnumerableProcessResult<IDTask>> GetListAsync(CancellationToken cancellationToken);
+        IProcessResult<IDTask> Insert(IDTask dTask);
+        Task<IProcessResult<IDTask>> InsertAsync(IDTask dTask);
+        Task<IProcessResult<IDTask>> InsertAsync(IDTask dTask, CancellationToken cancellationToken);
+        IProcessResult<IDTask> UpdateStatus(IDTask dTask);
+        Task<IProcessResult<IDTask>> UpdateStatusAsync(IDTask dTask);
+        Task<IProcessResult<IDTask>> UpdateStatusAsync(IDTask dTask, CancellationToken cancellationToken);
+    }
+}
