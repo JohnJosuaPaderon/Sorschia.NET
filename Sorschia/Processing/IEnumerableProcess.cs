7a847c4d22c2f64f05e7a06779e30d8c19a618c4
johnpad53 <johnjosua.paderon@outlook.com>
2017-09-27 09:09:39 +0800

    Added: IEnumerableProcess.cs; IProcess.cs;

---
+++ Sorschia/Processing/IEnumerableProcess.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Sorschia.Processing
+{
+    public interface IEnumerableProcess<T>
+    {
+        IEnumerableProcessResult<T> Execute();
+        Task<IEnumerableProcessResult<T>> ExecuteAsync();
+        Task<IEnumerableProcessResult<T>> ExecuteAsync(CancellationToken cancellationToken);
+    }
+}
---
+++ Sorschia/Processing/IProcess.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Sorschia.Processing
+{
+    public interface IProcess
+    {
+        IProcessResult Execute();
+        Task<IProcessResult> ExecuteAsync();
+        Task<IProcessResult> ExecuteAsync(CancellationToken cancellationToken);
+    }
+
+    public interface IProcess<T>
+    {
+        IProcessResult<T> Execute();
+        Task<IProcessResult<T>> ExecuteAsync();
+        Task<IProcessResult<T>> ExecuteAsync(CancellationToken cancellationToken);
+    }
+}
