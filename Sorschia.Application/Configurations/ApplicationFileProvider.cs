dce7e0f74229534626d5eee2d12af5c851a14e02
johnpad53 <johnjosua.paderon@outlook.com>
2017-09-27 09:10:12 +0800

    Added: ApplicationDirectoryProvider.cs; ApplicationFileProvider.cs;

---
+++ Sorschia.Application/Configurations/ApplicationFileProvider.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+
+namespace Sorschia.Configurations
+{
+    public sealed class ApplicationFileProvider
+    {
+        internal ApplicationFileProvider(SorschiaApp sorschiaApp)
+        {
+            _SorschiaApp = sorschiaApp;
+            _Files = new Dictionary<string, string>();
+        }
+
+        private readonly SorschiaApp _SorschiaApp;
+        private readonly Dictionary<string, string> _Files;
+
+        public string this[string key]
+        {
+            get
+            {
+                ValidateKey(key);
+
+                if (_Files.ContainsKey(key))
+                {
+                    return _SorschiaApp.ResolveRelativePath(_Files[key]);
+                }
+                else
+                {
+                    throw new SorschiaException(nameof(key), SorschiaExceptionType.KeyNotFound);
+                }
+            }
+            set
+            {
+                ValidateKey(key);
+
+                if (_Files.ContainsKey(key))
+                {
+                    _Files[key] = value;
+                }
+                else
+                {
+                    _Files.Add(key, value);
+                }
+            }
+        }
+
+        private void ValidateKey(string key)
+        {
+            if (string.IsNullOrWhiteSpace(key))
+            {
+                throw new SorschiaException(nameof(key), SorschiaExceptionType.KeyRequired);
+            }
+        }
+    }
+}
