ffa4f1b09f28cf90e5de26c32bc4dee75a7bae27
johnpad53 <johnjosua.paderon@outlook.com>
2017-09-27 16:23:51 +0800

    Added: App.config; App.xaml; App.xaml.cs; Bootstrapper.cs; logo.ico; packages.config; Sorschia.DailyTask.Desktop.csproj;

---
+++ Sorschia.DailyTask.Desktop/Bootstrapper.cs
@@ -0,0 +1,41 @@
+﻿using Prism.Unity;
+using Sorschia.DailyTask.Views;
+using System.Windows;
+
+namespace Sorschia.DailyTask
+{
+    class Bootstrapper : UnityBootstrapper
+    {
+        static Bootstrapper()
+        {
+            Current = new Bootstrapper();
+        }
+
+        public static Bootstrapper Current { get; }
+
+        public static void RunBootstrapper()
+        {
+            Current.Run();
+        }
+
+        private Bootstrapper()
+        {
+
+        }
+
+        protected override DependencyObject CreateShell()
+        {
+            return Container.TryResolve<MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+
+        protected override void ConfigureContainer()
+        {
+            base.ConfigureContainer();
+        }
+    }
+}
