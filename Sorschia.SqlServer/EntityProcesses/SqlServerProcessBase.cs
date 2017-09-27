341acb241016154c7c141651026af5d366cc1a0f
johnpad53 <johnjosua.paderon@outlook.com>
2017-09-27 09:11:18 +0800

    Added: SqlServerProcessBase.cs;

---
+++ Sorschia.SqlServer/EntityProcesses/SqlServerProcessBase.cs
@@ -0,0 +1,15 @@
+﻿using Sorschia.Data;
+using System.Data.SqlClient;
+
+namespace Sorschia.EntityProcesses
+{
+    public abstract class SqlServerProcessBase
+    {
+        public SqlServerProcessBase(IDbHelper<SqlConnection, SqlTransaction, SqlCommand, IQueryParameter> dbHelper)
+        {
+            _DbHelper = dbHelper;
+        }
+
+        protected readonly IDbHelper<SqlConnection, SqlTransaction, SqlCommand, IQueryParameter> _DbHelper;
+    }
+}
