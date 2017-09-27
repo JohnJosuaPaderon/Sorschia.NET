2e4ae02d8c4c2dc44775cd023403b27b7e37b807
johnpad53 <johnjosua.paderon@outlook.com>
2017-09-27 09:10:23 +0800

    Updated: NameBase.cs;

--- Sorschia.Application/Entities/NameBase.cs
+++ Sorschia.Application/Entities/NameBase.cs
@@ -6,9 +6,9 @@ namespace Sorschia.Entities
     {
         static NameBase()
         {
-            _AcronymBuilder = SorschiaApp.Resolve<IAcronymBuilder>() ?? throw new SorschiaException(nameof(IAcronymBuilder), SorschiaExceptionType.ServiceRequired);
-            _FullNameBuilder = SorschiaApp.Resolve<IFullNameBuilder>() ?? throw new SorschiaException(nameof(IFullNameBuilder), SorschiaExceptionType.ServiceRequired);
-            _InformalFullNameBuilder = SorschiaApp.Resolve<IInformalFullNameBuilder>() ?? throw new SorschiaException(nameof(IInformalFullNameBuilder), SorschiaExceptionType.ServiceRequired);
+            _AcronymBuilder = SorschiaApp.ResolveService<IAcronymBuilder>() ?? throw new SorschiaException(nameof(IAcronymBuilder), SorschiaExceptionType.ServiceRequired);
+            _FullNameBuilder = SorschiaApp.ResolveService<IFullNameBuilder>() ?? throw new SorschiaException(nameof(IFullNameBuilder), SorschiaExceptionType.ServiceRequired);
+            _InformalFullNameBuilder = SorschiaApp.ResolveService<IInformalFullNameBuilder>() ?? throw new SorschiaException(nameof(IInformalFullNameBuilder), SorschiaExceptionType.ServiceRequired);
         }
 
         private static readonly IAcronymBuilder _AcronymBuilder;
