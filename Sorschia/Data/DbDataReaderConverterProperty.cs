bc9ec719f65b6a10bd38832064d28ae775f725bb
johnpad53 <johnjosua.paderon@outlook.com>
2017-10-02 15:13:45 +0800

    Updated: DbDataReaderConverterProperty.cs;

--- Sorschia/Data/DbDataReaderConverterProperty.cs
+++ Sorschia/Data/DbDataReaderConverterProperty.cs
@@ -2,8 +2,19 @@
 {
     public class DbDataReaderConverterProperty<T> : IDbDataReaderConverterProperty<T>
     {
+        private T _Value;
+
         public bool UseProvidedValue { get; set; }
-        public T Value { get; set; }
+
+        public T Value
+        {
+            get { return _Value; }
+            set
+            {
+                UseProvidedValue = false;
+                _Value = value;
+            }
+        }
 
         public T TryGetValue(T alternativeValue)
         {
