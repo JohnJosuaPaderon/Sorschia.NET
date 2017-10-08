c06d7010f3ed11a41219f4a40b6f61ba2b0d1257
johnpad53 <johnjosua.paderon@outlook.com>
2017-10-08 12:10:46 +0800

    Added: DefaultIntegrator.cs;

---
+++ Sorschia.DailyTask/Extensions/DefaultIntegrator.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Sorschia.DailyTask.EntityInfo;
+
+namespace Sorschia.DailyTask.Extensions
+{
+    public static class DefaultIntegrator
+    {
+        public static IServiceCollection UseDailyTaskDefaultInfoProviders(this IServiceCollection instance)
+        {
+            instance.AddSingleton<IDTaskFields, DTaskFields>();
+            instance.AddSingleton<IDTaskParameters, DTaskParameters>();
+
+            return instance;
+        }
+    }
+}
