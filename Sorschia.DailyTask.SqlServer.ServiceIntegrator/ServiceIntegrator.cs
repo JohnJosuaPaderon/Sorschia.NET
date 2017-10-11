3612f7091648da61778e5821ebd055ea11c64e78
johnpad53 <johnjosua.paderon@outlook.com>
2017-10-11 17:57:32 +0800

    Added: ServiceIntegrator.cs;
    Added Project: Sorschia.DailyTask.SqlServer.ServiceIntegrator

---
+++ Sorschia.DailyTask.SqlServer.ServiceIntegrator/ServiceIntegrator.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Sorschia.DailyTask.Extensions;
+using System.Composition;
+
+namespace Sorschia.DailyTask
+{
+    [Export(typeof(IServiceIntegrator))]
+    public sealed class ServiceIntegrator : IServiceIntegrator
+    {
+        public IServiceCollection Integrate(IServiceCollection services)
+        {
+            services.UseSqlServerForDailyTask();
+
+            return services;
+        }
+    }
+}
