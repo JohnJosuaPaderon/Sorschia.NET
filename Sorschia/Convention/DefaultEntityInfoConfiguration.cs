c0204dc9ab56702bfda988db3feb1e7b1c816c8b
john josua paderon <johnjosua.paderon@outlook.com>
2017-11-06 12:38:14 +0800

    Added: DefaultEntityInfoConfiguration.cs;

---
+++ Sorschia/Convention/DefaultEntityInfoConfiguration.cs
@@ -0,0 +1,14 @@
+﻿namespace Sorschia.Convention
+{
+    public sealed class DefaultEntityInfoConfiguration : IEntityInfoConfiguration
+    {
+        public DefaultEntityInfoConfiguration()
+        {
+            FieldPrefix = string.Empty;
+            ParameterPrefix = "@_";
+        }
+
+        public string FieldPrefix { get; }
+        public string ParameterPrefix { get; }
+    }
+}
