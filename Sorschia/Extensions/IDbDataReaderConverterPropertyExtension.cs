fa341f79c450d9b81e7396a5bdd87a0e02566daf
john josua paderon <johnjosua.paderon@outlook.com>
2017-11-06 12:40:11 +0800

    Added: IServiceCollectionExtension.Definitions.Default.cs;
    Updated: IDbDataReaderConverterPropertyExtension.cs; IServiceCollectionExtension.cs;

--- Sorschia/Extensions/IDbDataReaderConverterPropertyExtension.cs
+++ Sorschia/Extensions/IDbDataReaderConverterPropertyExtension.cs
@@ -1,5 +1,8 @@
 ﻿using Sorschia.Data;
+using Sorschia.Processing;
 using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Sorschia.Extensions
 {
@@ -7,7 +10,22 @@ namespace Sorschia.Extensions
     {
         public static T TryGetValue<T>(this IDbDataReaderConverterProperty<T> instance, Func<string, T> expression, string arg)
         {
-            return instance.TryGetValue(expression(arg));
+            return instance.UseProvidedValue ? instance.Value : expression(arg);
+        }
+
+        public static TResult TryGetValueFromProcess<TResult, TArg>(this IDbDataReaderConverterProperty<TResult> instance, Func<TArg, IProcessResult<TResult>> getDataById, Func<string, TArg> getIdFromReader, string idField)
+        {
+            return instance.UseProvidedValue ? instance.Value : getDataById(getIdFromReader(idField)).Data;
+        }
+
+        public static async Task<TResult> TryGetValueFromProcessAsync<TResult, TArg>(this IDbDataReaderConverterProperty<TResult> instance, Func<TArg, Task<IProcessResult<TResult>>> getDataByIdAsync, Func<string, TArg> getIdFromReader, string idField)
+        {
+            return instance.UseProvidedValue ? instance.Value : (await getDataByIdAsync(getIdFromReader(idField))).Data;
+        }
+
+        public static async Task<TResult> TryGetValueFromProcessAsync<TResult, TArg>(this IDbDataReaderConverterProperty<TResult> instance, Func<TArg, CancellationToken, Task<IProcessResult<TResult>>> getDataByIdAsync, Func<string, TArg> getIdFromReader, string idField, CancellationToken cancellationToken)
+        {
+            return instance.UseProvidedValue ? instance.Value : (await getDataByIdAsync(getIdFromReader(idField), cancellationToken)).Data;
         }
     }
 }
---
+++ Sorschia/Extensions/IServiceCollectionExtension.Definitions.Default.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Sorschia.Convention;
+using Sorschia.Data;
+
+namespace Sorschia.Extensions
+{
+    partial class IServiceCollectionExtension
+    {
+        public static IServiceCollection UseCoreDefault(this IServiceCollection instance)
+        {
+            instance.UseDefaultEntityInfoConfiguration();
+            instance.UseDefaultDbDataReaderToProcessResultConverter();
+            instance.UseDefaultDbDataReaderToEnumerableProcessResultConverter();
+
+            return instance;
+        }
+
+        public static IServiceCollection UseDefaultDbDataReaderToProcessResultConverter(this IServiceCollection instance)
+        {
+            instance.UseDbDataReaderToProcessResultConverter<DefaultDbDataReaderToProcessResultConverter>();
+
+            return instance;
+        }
+
+        public static IServiceCollection UseDefaultDbDataReaderToEnumerableProcessResultConverter(this IServiceCollection instance)
+        {
+            instance.UseDbDataReaderToEnumerableProcessResultConverter<DefaultDbDataReaderToEnumerableProcessResultConverter>();
+
+            return instance;
+        }
+
+        public static IServiceCollection UseDefaultEntityInfoConfiguration(this IServiceCollection instance)
+        {
+            instance.UseEntityInfoConfiguration<DefaultEntityInfoConfiguration>();
+
+            return instance;
+        }
+    }
+}
