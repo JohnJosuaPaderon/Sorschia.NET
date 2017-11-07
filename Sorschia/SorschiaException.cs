2a33d5d6040df27127001bd0191d8ce151ba3899
john josua paderon <johnjosua.paderon@outlook.com>
2017-11-07 09:48:53 +0800

    Updated: Sorschia.csproj; SorschiaException.cs;

--- Sorschia/SorschiaException.cs
+++ Sorschia/SorschiaException.cs
@@ -5,6 +5,11 @@ namespace Sorschia
     [Serializable]
     public sealed class SorschiaException : Exception
     {
+        public static SorschiaException ServiceRequired(string serviceName)
+        {
+            return new SorschiaException($"The service '{serviceName}' is required by the application.", SorschiaExceptionKind.ServiceRequired);
+        }
+
         public SorschiaException(SorschiaExceptionKind kind = SorschiaExceptionKind.Undefined)
         {
             Kind = kind;
