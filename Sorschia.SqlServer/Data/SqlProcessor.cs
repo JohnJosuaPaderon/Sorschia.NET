ba6ba81e74e3b8a59ee10c45baa2e9a6eb727928
john josua paderon <johnjosua.paderon@outlook.com>
2017-11-29 15:13:19 +0800

    Added: SqlProcessor.cs;

---
+++ Sorschia.SqlServer/Data/SqlProcessor.cs
@@ -0,0 +1,11 @@
+﻿using System.Data.SqlClient;
+
+namespace Sorschia.Data
+{
+    public sealed class SqlProcessor : DbProcessorBase<SqlCommand>, IDbProcessor<SqlCommand>
+    {
+        public SqlProcessor(IDbCommandCreator<SqlCommand> commandCreator) : base(commandCreator)
+        {
+        }
+    }
+}
