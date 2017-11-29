0d4ac23527b942bf32acce335e7c875e24090a4e
john josua paderon <johnjosua.paderon@outlook.com>
2017-11-29 14:36:13 +0800

    Added: DbProcessContextFactory.cs;

---
+++ Sorschia/Processing/DbProcessContextFactory.cs
@@ -0,0 +1,30 @@
+﻿using Sorschia.Data;
+using System.Data.Common;
+
+namespace Sorschia.Processing
+{
+    public sealed class DbProcessContextFactory<TConnection, TTransaction> : IProcessContextFactory
+        where TConnection : DbConnection
+        where TTransaction : DbTransaction
+    {
+        public DbProcessContextFactory(IDbConnectionProvider<TConnection> connectionProvider, IDbTransactionProvider<TTransaction> transactionProvider)
+        {
+            _ConnectionProvider = connectionProvider;
+            _TransactionProvider = transactionProvider;
+        }
+
+        private readonly IDbConnectionProvider<TConnection> _ConnectionProvider;
+        private readonly IDbTransactionProvider<TTransaction> _TransactionProvider;
+
+        public void Finish(IProcessContext context)
+        {
+            _ConnectionProvider.CloseDispose(context);
+            _TransactionProvider.Dispose(context);
+        }
+
+        public IProcessContext Generate()
+        {
+            return new DbProcessContext();
+        }
+    }
+}
