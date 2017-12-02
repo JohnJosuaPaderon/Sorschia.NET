131a4939d509260a5c1119be0c830c2edda0c11a
john josua paderon <johnjosua.paderon@outlook.com>
2017-12-02 15:37:49 +0800

    Added: CustomPopupWindowAction.cs; PopupViewModelBase.cs; ViewModelBase.cs;

---
+++ Sorschia.Wpf/ViewModels/CustomPopupWindowAction.cs
@@ -0,0 +1,25 @@
+﻿using Prism.Interactivity;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Sorschia.ViewModels
+{
+    public class CustomPopupWindowAction : PopupWindowAction
+    {
+        protected override Window CreateWindow()
+        {
+            return new Window()
+            {
+                SizeToContent = SizeToContent.WidthAndHeight,
+                //ShowTitleBar = false,
+                //ShowMinButton = false,
+                //ShowMaxRestoreButton = false,
+                //ShowCloseButton = false,
+                WindowStartupLocation = WindowStartupLocation ?? System.Windows.WindowStartupLocation.CenterScreen,
+                BorderBrush = new SolidColorBrush(Color.FromArgb(50, 0, 0, 0)),
+                BorderThickness = new Thickness(1),
+                Padding = new Thickness(50)
+            };
+        }
+    }
+}
---
+++ Sorschia.Wpf/ViewModels/ViewModelBase.cs
@@ -0,0 +1,36 @@
+﻿using Sorschia.Events;
+using Prism.Commands;
+using Prism.Events;
+using Prism.Mvvm;
+using System.Windows.Input;
+
+namespace Sorschia.ViewModels
+{
+    public abstract class ViewModelBase : BindableBase
+    {
+        public ViewModelBase(IEventAggregator eventAggregator)
+        {
+            _EventAggregator = eventAggregator;
+            _WindowTitleEvent = eventAggregator.GetEvent<WindowTitleEvent>();
+
+            InitializeCommand = new DelegateCommand(Initialize);
+            ResetMouseCaptureCommand = new DelegateCommand(ResetMouseCapture);
+        }
+
+        protected readonly IEventAggregator _EventAggregator;
+        protected readonly WindowTitleEvent _WindowTitleEvent;
+
+        public DelegateCommand ResetMouseCaptureCommand { get; }
+        public DelegateCommand InitializeCommand { get; }
+
+        private void ResetMouseCapture()
+        {
+            Mouse.Capture(null);
+        }
+
+        protected virtual void Initialize()
+        {
+
+        }
+    }
+}
