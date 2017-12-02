19e0cf42f5080cf3dfbeda70b76ad3f3b11324d6
john josua paderon <johnjosua.paderon@outlook.com>
2017-12-02 22:21:54 +0800

    Added: DbDataReaderConverterBase.cs; DbDataReaderConverterProperty.cs; IDbDataReaderConverterProperty.cs;

---
+++ Sorschia/Data/DbDataReaderConverterBase.cs
@@ -0,0 +1,241 @@
+﻿using Sorschia.Processing;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Sorschia.Data
+{
+    public abstract class DbDataReaderConverterBase<T>
+    {
+        public IProcessResult<T> FromReader(DbDataReader reader, Func<DbDataReader, T> converter)
+        {
+            if (reader.HasRows)
+            {
+                try
+                {
+                    reader.Read();
+                    return ProcessResult<T>.Success(converter(reader));
+                }
+                catch (Exception ex)
+                {
+                    return ProcessResult<T>.Failed(ex);
+                }
+            }
+            else
+            {
+                return ProcessResult<T>.NoResult();
+            }
+        }
+
+        public async Task<IProcessResult<T>> FromReaderAsync(DbDataReader reader, Func<DbDataReader, Task<T>> convertAsync)
+        {
+            if (reader.HasRows)
+            {
+                try
+                {
+                    await reader.ReadAsync();
+                    return ProcessResult<T>.Success(await convertAsync(reader));
+                }
+                catch (Exception ex)
+                {
+                    return ProcessResult<T>.Failed(ex);
+                }
+            }
+            else
+            {
+                return ProcessResult<T>.NoResult();
+            }
+        }
+
+        public async Task<IProcessResult<T>> FromReaderAsync(DbDataReader reader, CancellationToken cancellationToken, Func<DbDataReader, CancellationToken, Task<T>> convertAsync)
+        {
+            if (reader.HasRows)
+            {
+                try
+                {
+                    await reader.ReadAsync(cancellationToken);
+                    return ProcessResult<T>.Success(await convertAsync(reader, cancellationToken));
+                }
+                catch (Exception ex)
+                {
+                    return ProcessResult<T>.Failed(ex);
+                }
+            }
+            else
+            {
+                return ProcessResult<T>.NoResult();
+            }
+        }
+
+        public async Task<IProcessResult<T>> FromReaderAsync(DbDataReader reader, Func<DbDataReader, T> convert)
+        {
+            if (reader.HasRows)
+            {
+                try
+                {
+                    await reader.ReadAsync();
+                    return ProcessResult<T>.Success(convert(reader));
+                }
+                catch (Exception ex)
+                {
+                    return ProcessResult<T>.Failed(ex);
+                }
+            }
+            else
+            {
+                return ProcessResult<T>.NoResult();
+            }
+        }
+
+        public async Task<IProcessResult<T>> FromReaderAsync(DbDataReader reader, CancellationToken cancellationToken, Func<DbDataReader, T> convert)
+        {
+            if (reader.HasRows)
+            {
+                try
+                {
+                    await reader.ReadAsync(cancellationToken);
+                    return ProcessResult<T>.Success(convert(reader));
+                }
+                catch (Exception ex)
+                {
+                    return ProcessResult<T>.Failed(ex);
+                }
+            }
+            else
+            {
+                return ProcessResult<T>.NoResult();
+            }
+        }
+        public IEnumerableProcessResult<T> EnumerableFromReader(DbDataReader reader, Func<DbDataReader, T> convert)
+        {
+            if (reader.HasRows)
+            {
+                try
+                {
+                    var list = new List<T>();
+
+                    while (reader.Read())
+                    {
+                        list.Add(convert(reader));
+                    }
+
+                    return EnumerableProcessResult<T>.Success(list);
+                }
+                catch (Exception ex)
+                {
+                    return EnumerableProcessResult<T>.Failed(ex);
+                }
+            }
+            else
+            {
+                return EnumerableProcessResult<T>.NoResult();
+            }
+        }
+
+        public async Task<IEnumerableProcessResult<T>> EnumerableFromReaderAsync(DbDataReader reader, Func<DbDataReader, Task<T>> convertAsync)
+        {
+            if (reader.HasRows)
+            {
+                try
+                {
+                    var list = new List<T>();
+
+                    while (await reader.ReadAsync())
+                    {
+                        list.Add(await convertAsync(reader));
+                    }
+
+                    return EnumerableProcessResult<T>.Success(list);
+                }
+                catch (Exception ex)
+                {
+                    return EnumerableProcessResult<T>.Failed(ex);
+                }
+            }
+            else
+            {
+                return EnumerableProcessResult<T>.NoResult();
+            }
+        }
+
+        public async Task<IEnumerableProcessResult<T>> EnumerableFromReaderAsync(DbDataReader reader, CancellationToken cancellationToken, Func<DbDataReader, CancellationToken, Task<T>> convertAsync)
+        {
+            if (reader.HasRows)
+            {
+                try
+                {
+                    var list = new List<T>();
+
+                    while (await reader.ReadAsync(cancellationToken))
+                    {
+                        list.Add(await convertAsync(reader, cancellationToken));
+                    }
+
+                    return EnumerableProcessResult<T>.Success(list);
+                }
+                catch (Exception ex)
+                {
+                    return EnumerableProcessResult<T>.Failed(ex);
+                }
+            }
+            else
+            {
+                return EnumerableProcessResult<T>.NoResult();
+            }
+        }
+
+        public async Task<IEnumerableProcessResult<T>> EnumerableFromReaderAsync(DbDataReader reader, Func<DbDataReader, T> convert)
+        {
+            if (reader.HasRows)
+            {
+                try
+                {
+                    var list = new List<T>();
+
+                    while (await reader.ReadAsync())
+                    {
+                        list.Add(convert(reader));
+                    }
+
+                    return EnumerableProcessResult<T>.Success(list);
+                }
+                catch (Exception ex)
+                {
+                    return EnumerableProcessResult<T>.Failed(ex);
+                }
+            }
+            else
+            {
+                return EnumerableProcessResult<T>.NoResult();
+            }
+        }
+
+        public async Task<IEnumerableProcessResult<T>> EnumerableFromReaderAsync(DbDataReader reader, CancellationToken cancellationToken, Func<DbDataReader, T> convert)
+        {
+            if (reader.HasRows)
+            {
+                try
+                {
+                    var list = new List<T>();
+
+                    while (await reader.ReadAsync(cancellationToken))
+                    {
+                        list.Add(convert(reader));
+                    }
+
+                    return EnumerableProcessResult<T>.Success(list);
+                }
+                catch (Exception ex)
+                {
+                    return EnumerableProcessResult<T>.Failed(ex);
+                }
+            }
+            else
+            {
+                return EnumerableProcessResult<T>.NoResult();
+            }
+        }
+    }
+}
---
+++ Sorschia/Data/DbDataReaderConverterProperty.cs
@@ -0,0 +1,24 @@
+﻿namespace Sorschia.Data
+{
+    public class DbDataReaderConverterProperty<T> : IDbDataReaderConverterProperty<T>
+    {
+        private T _Value;
+
+        public bool UseProvidedValue { get; set; }
+
+        public T Value
+        {
+            get { return _Value; }
+            set
+            {
+                UseProvidedValue = true;
+                _Value = value;
+            }
+        }
+
+        public T TryGetValue(T alternativeValue)
+        {
+            return UseProvidedValue ? Value : alternativeValue;
+        }
+    }
+}
