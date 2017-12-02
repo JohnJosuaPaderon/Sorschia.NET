e52dbc9b7fc438746f950e20025a46103dc27180
john josua paderon <johnjosua.paderon@outlook.com>
2017-12-02 15:34:57 +0800

    Added: DbQueryParameterDirectionConverter.cs;

---
+++ Sorschia/Data/DbQueryParameterDirectionConverter.cs
@@ -0,0 +1,22 @@
+﻿using System.Data;
+
+namespace Sorschia.Data
+{
+    public static class DbQueryParameterDirectionConverter
+    {
+        public static ParameterDirection Convert(DbQueryParameterDirection value)
+        {
+            switch (value)
+            {
+                case DbQueryParameterDirection.In:
+                    return ParameterDirection.Input;
+                case DbQueryParameterDirection.Out:
+                    return ParameterDirection.Output;
+                case DbQueryParameterDirection.InOut:
+                    return ParameterDirection.InputOutput;
+                default:
+                    return ParameterDirection.Input;
+            }
+        }
+    }
+}
