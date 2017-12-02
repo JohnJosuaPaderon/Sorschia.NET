1b8526897ca22ef9d18f94617348d30f93c70724
john josua paderon <johnjosua.paderon@outlook.com>
2017-12-02 15:36:24 +0800

    Added: SqlEntityManagerBase.cs;

---
+++ Sorschia.SqlServer/Entity/Manager/SqlEntityManagerBase.cs
@@ -0,0 +1,34 @@
+﻿using Sorschia.Entities;
+using Sorschia.Processing;
+
+namespace Sorschia.Entity.Manager
+{
+    public abstract class SqlEntityManagerBase<T, TIdentifier> : EntityManagerBase<T, TIdentifier>
+        where T : IEntity<TIdentifier>
+    {
+        public SqlEntityManagerBase(IProcessContextFactory contextFactory, string connectionStringKey)
+        {
+            _ContextFactory = contextFactory;
+            ConnectionStringKey = connectionStringKey;
+        }
+
+        private readonly IProcessContextFactory _ContextFactory;
+
+        protected IProcessContext GenerateContext()
+        {
+            var context = _ContextFactory.Generate();
+
+            if (context is DbProcessContext dbProcessContext)
+            {
+                dbProcessContext.ConnectionStringKey = ConnectionStringKey;
+                return context; 
+            }
+            else
+            {
+                throw SorschiaException.InvalidOperation($"Type of '{typeof(DbProcessContext).FullName}' is expected.");
+            }
+        }
+
+        private readonly string ConnectionStringKey;
+    }
+}
