46e4b7c1f1de9bb99844039e6d4bbda7630c673a
john josua paderon <johnjosua.paderon@outlook.com>
2017-12-02 22:22:49 +0800

    Added: EntityConverterBase.cs; IEntityConverter.cs;

---
+++ Sorschia/Entity/Converter/EntityConverterBase.cs
@@ -0,0 +1,15 @@
+﻿using Sorschia.Data;
+
+namespace Sorschia.Entity.Converter
+{
+    public abstract class EntityConverterBase<T, TIdentifier>
+        where T : IEntity<TIdentifier>
+    {
+        public EntityConverterBase()
+        {
+            PId = new DbDataReaderConverterProperty<TIdentifier>();
+        }
+
+        public IDbDataReaderConverterProperty<TIdentifier> PId { get; }
+    }
+}
---
+++ Sorschia/Entity/Converter/IEntityConverter.cs
@@ -0,0 +1,10 @@
+﻿using Sorschia.Data;
+
+namespace Sorschia.Entity.Converter
+{
+    public interface IEntityConverter<T, TIdentifier> : IDbDataReaderConverter<T>
+        where T : IEntity<TIdentifier>
+    {
+        IDbDataReaderConverterProperty<TIdentifier> PId { get; }
+    }
+}
