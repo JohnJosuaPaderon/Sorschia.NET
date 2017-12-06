16363544472d53b92e0939a08361ba26af9e02e2
john josua paderon <johnjosua.paderon@outlook.com>
2017-12-06 14:04:23 +0800

    Added: SorschiaPopupWindowAction.cs;
    Updated: ViewModelBase.cs;
    
    Removed: CustomPopupWindowAction.cs;

---
+++ Sorschia.Wpf/ViewModels/SorschiaPopupWindowAction.cs
@@ -0,0 +1,25 @@
+﻿using MahApps.Metro.Controls;
+using Prism.Interactivity;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Sorschia.ViewModels
+{
+    public class SorschiaPopupWindowAction : PopupWindowAction
+    {
+        protected override Window CreateWindow()
+        {
+            return new MetroWindow()
+            {
+                SizeToContent = SizeToContent.WidthAndHeight,
+                ShowTitleBar = false,
+                ShowMinButton = false,
+                ShowMaxRestoreButton = false,
+                ShowCloseButton = false,
+                WindowStartupLocation = WindowStartupLocation ?? System.Windows.WindowStartupLocation.CenterOwner,
+                BorderBrush = new SolidColorBrush(Color.FromArgb(50, 0, 0, 0)),
+                BorderThickness = new Thickness(1)
+            };
+        }
+    }
+}
--- Sorschia.Wpf/ViewModels/ViewModelBase.cs
+++ Sorschia.Wpf/ViewModels/ViewModelBase.cs
@@ -1,24 +1,23 @@
-﻿using Sorschia.Events;
-using Prism.Commands;
+﻿using Prism.Commands;
 using Prism.Events;
 using Prism.Mvvm;
+using Prism.Regions;
 using System.Windows.Input;
 
 namespace Sorschia.ViewModels
 {
-    public abstract class ViewModelBase : BindableBase
+    public abstract class ViewModelBase : BindableBase, INavigationAware
     {
-        public ViewModelBase(IEventAggregator eventAggregator)
+        public ViewModelBase(IRegionManager regionManager, IEventAggregator eventAggregator)
         {
             _EventAggregator = eventAggregator;
-            _WindowTitleEvent = eventAggregator.GetEvent<WindowTitleEvent>();
 
             InitializeCommand = new DelegateCommand(Initialize);
             ResetMouseCaptureCommand = new DelegateCommand(ResetMouseCapture);
         }
 
+        protected readonly IRegionManager _RegionManager;
         protected readonly IEventAggregator _EventAggregator;
-        protected readonly WindowTitleEvent _WindowTitleEvent;
 
         public DelegateCommand ResetMouseCaptureCommand { get; }
         public DelegateCommand InitializeCommand { get; }
@@ -31,6 +30,19 @@ namespace Sorschia.ViewModels
         protected virtual void Initialize()
         {
 
+        }
+
+        public virtual void OnNavigatedTo(NavigationContext navigationContext)
+        {
+        }
+
+        public virtual bool IsNavigationTarget(NavigationContext navigationContext)
+        {
+            return true;
+        }
+
+        public virtual void OnNavigatedFrom(NavigationContext navigationContext)
+        {
         }
     }
 }
