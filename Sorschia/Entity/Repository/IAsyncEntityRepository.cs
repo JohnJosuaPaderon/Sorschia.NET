e147ec7e58453ebaf107fe6844aef04ee9c6cad3
john josua paderon <johnjosua.paderon@outlook.com>
2017-12-22 14:27:32 +0800

    Added: IAsyncEntityRepository.cs; IEntityRepository.cs;

---
+++ Sorschia/Entity/Repository/IAsyncEntityRepository.cs
@@ -0,0 +1,13 @@
+﻿using Sorschia.Repository;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Sorschia.Entity.Repository
+{
+    public interface IAsyncEntityRepository<T, TIdentifier> : IAsyncRepository<T>
+        where T : IEntity<TIdentifier>
+    {
+        Task<T> GetAsync(TIdentifier id);
+        Task<T> GetAsync(TIdentifier id, CancellationToken cancellationToken);
+    }
+}
---
+++ Sorschia/Entity/Repository/IEntityRepository.cs
@@ -0,0 +1,10 @@
+﻿using Sorschia.Repository;
+
+namespace Sorschia.Entity.Repository
+{
+    public interface IEntityRepository<T, TIdentifier> : IRepository<T>
+        where T : IEntity<TIdentifier>
+    {
+        T Get(TIdentifier id);
+    }
+}
