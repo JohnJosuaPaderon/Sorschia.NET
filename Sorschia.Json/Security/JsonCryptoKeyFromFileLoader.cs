9796ca4818b50540fd597dcb354abb58f416c820
john josua paderon <johnjosua.paderon@outlook.com>
2017-12-22 14:28:45 +0800

    Added: JsonCryptoKeyFromFileLoader.cs;

---
+++ Sorschia.Json/Security/JsonCryptoKeyFromFileLoader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sorschia.Security
+{
+    public sealed class JsonCryptoKeyFromFileLoader : ICryptoKeyLoader
+    {
+
+
+        public void Load(ICryptoKeyProvider recipient)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
