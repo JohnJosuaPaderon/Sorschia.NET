b770de4f436829dd46fd338a957ddc620d164129
john josua paderon <johnjosua.paderon@outlook.com>
2017-12-23 16:25:15 +0800

    Added: WindowTitleBarVisibilityEvent.cs;

---
+++ Sorschia.Wpf/Events/WindowTitleBarVisibilityEvent.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace Sorschia.Events
+{
+    public sealed class WindowTitleBarVisibilityEvent : SorschiaPubSubEventBase<Visibility>
+    {
+        void Show()
+        {
+            Change(Visibility.Visible);
+        }
+
+        void Collapse()
+        {
+            Change(Visibility.Collapsed);
+        }
+
+        void Hide()
+        {
+            Change(Visibility.Hidden);
+        }
+    }
+}
