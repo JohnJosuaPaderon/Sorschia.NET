cf9822061856b06f8429ab8331feb0d0f59b33c5
john josua paderon <johnjosua.paderon@outlook.com>
2018-01-20 16:49:36 +0800

    Updated: EntityManagerBase.cs;

--- Sorschia/Entity/Manager/EntityManagerBase.cs
+++ Sorschia/Entity/Manager/EntityManagerBase.cs
@@ -6,68 +6,68 @@ namespace Sorschia.Entity.Manager
     public abstract class EntityManagerBase<T, TIdentifier>
         where T : IEntity<TIdentifier>
     {
-        static EntityManagerBase()
+        public EntityManagerBase()
         {
-            StaticSource = new EntityCollection<T, TIdentifier>();
+            Source = new EntityCollection<T, TIdentifier>();
         }
 
-        protected static IEntityCollection<T, TIdentifier> StaticSource { get; }
+        protected IEntityCollection<T, TIdentifier> Source { get; }
 
-        protected IProcessResult<T> AddIfSuccess(IProcessResult<T> result)
+        protected IProcessResult<T> TryAdd(IProcessResult<T> result)
         {
-            return InvokeIfSuccess(result, () => StaticSource.Add(result.Data));
+            return TryInvoke(result, () => Source.Add(result.Data));
         }
 
-        protected IEnumerableProcessResult<T> AddIfSuccess(IEnumerableProcessResult<T> result)
+        protected IEnumerableProcessResult<T> TryAdd(IEnumerableProcessResult<T> result)
         {
-            return InvokeIfSuccess(result, () => StaticSource.Add(result.DataCollection));
+            return InvokeIfSuccess(result, () => Source.Add(result.DataCollection));
         }
 
-        protected IProcessResult<T> AddUpdateIfSuccess(IProcessResult<T> result)
+        protected IProcessResult<T> TryAddUpdate(IProcessResult<T> result)
         {
-            return InvokeIfSuccess(result, () => StaticSource.AddUpdate(result.Data));
+            return TryInvoke(result, () => Source.AddUpdate(result.Data));
         }
 
-        protected IEnumerableProcessResult<T> AddUpdateIfSuccess(IEnumerableProcessResult<T> result)
+        protected IEnumerableProcessResult<T> TryAddUpdate(IEnumerableProcessResult<T> result)
         {
-            return InvokeIfSuccess(result, () => StaticSource.AddUpdate(result.DataCollection));
+            return InvokeIfSuccess(result, () => Source.AddUpdate(result.DataCollection));
         }
 
-        protected IProcessResult<T> RemoveIfSuccess(IProcessResult<T> result)
+        protected IProcessResult<T> TryRemove(IProcessResult<T> result)
         {
-            return InvokeIfSuccess(result, () => StaticSource.Remove(result.Data));
+            return TryInvoke(result, () => Source.Remove(result.Data));
         }
 
-        protected IEnumerableProcessResult<T> RemoveIfSuccess(IEnumerableProcessResult<T> result)
+        protected IEnumerableProcessResult<T> TryRemove(IEnumerableProcessResult<T> result)
         {
-            return InvokeIfSuccess(result, () => StaticSource.Remove(result.DataCollection));
+            return InvokeIfSuccess(result, () => Source.Remove(result.DataCollection));
         }
 
-        protected IProcessResult<T> UpdateIfSuccess(IProcessResult<T> result)
+        protected IProcessResult<T> TryUpdate(IProcessResult<T> result)
         {
-            return InvokeIfSuccess(result, () => StaticSource.Update(result.Data));
+            return TryInvoke(result, () => Source.Update(result.Data));
         }
 
-        protected IEnumerableProcessResult<T> UpdateIfSuccess(IEnumerableProcessResult<T> result)
+        protected IEnumerableProcessResult<T> TryUpdate(IEnumerableProcessResult<T> result)
         {
-            return InvokeIfSuccess(result, () => StaticSource.Update(result.DataCollection));
+            return InvokeIfSuccess(result, () => Source.Update(result.DataCollection));
         }
 
-        protected IProcessResult<T> InvokeIfSuccess(IProcessResult<T> result, Action expression)
+        protected IProcessResult<T> TryInvoke(IProcessResult<T> result, Action callback)
         {
             if (result.Status == ProcessResultStatus.Success)
             {
-                expression?.Invoke();
+                callback?.Invoke();
             }
 
             return result;
         }
 
-        protected IEnumerableProcessResult<T> InvokeIfSuccess(IEnumerableProcessResult<T> result, Action expression)
+        protected IEnumerableProcessResult<T> InvokeIfSuccess(IEnumerableProcessResult<T> result, Action callback)
         {
             if (result.Status == ProcessResultStatus.Success)
             {
-                expression?.Invoke();
+                callback?.Invoke();
             }
 
             return result;
