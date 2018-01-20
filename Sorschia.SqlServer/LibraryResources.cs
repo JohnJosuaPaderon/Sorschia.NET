ab885dbe7d2418cade4a1359c31df18d31d5ef96
john josua paderon <johnjosua.paderon@outlook.com>
2018-01-20 16:49:24 +0800

    Added: LibraryResources.cs;

---
+++ Sorschia.SqlServer/LibraryResources.cs
@@ -0,0 +1,14 @@
+﻿namespace Sorschia
+{
+    internal static class LibraryResources
+    {
+        static LibraryResources()
+        {
+            DefaultConnectionStringKey = "dbo";
+            DefaultSchema = "dbo";
+        }
+
+        public static string DefaultConnectionStringKey { get; }
+        public static string DefaultSchema { get; }
+    }
+}
