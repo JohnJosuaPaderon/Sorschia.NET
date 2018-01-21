2dff8fd84cb05fc2c81c5dcf1bedb8daf013f6dd
john josua paderon <johnjosua.paderon@outlook.com>
2018-01-21 19:12:51 +0800

    Added: AssignmentStatusManager.cs; CoreEntityManagerBase.cs; IServiceCollectionExtension.cs;

---
+++ MyDayManager.SqlServer/Entity/Manager/CoreEntityManagerBase.cs
@@ -0,0 +1,14 @@
+﻿using Sorschia.Entity;
+using Sorschia.Entity.Manager;
+using Sorschia.Processing;
+
+namespace MyDayManager.Entity.Manager
+{
+    public abstract class CoreEntityManagerBase<T, TIdentifier> : SqlEntityManagerBase<T, TIdentifier>
+        where T : IEntity<TIdentifier>
+    {
+        public CoreEntityManagerBase(IProcessContextFactory contextFactory) : base(contextFactory)
+        {
+        }
+    }
+}
---
+++ MyDayManager.SqlServer/Entity/Manager/IServiceCollectionExtension.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace MyDayManager.Entity.Manager
+{
+    public static class IServiceCollectionExtension
+    {
+        public static IServiceCollection UseMyDayManagerEntityManager(this IServiceCollection instance)
+        {
+            return instance
+                .AddSingleton<IAssignmentStatusManager, AssignmentStatusManager>();
+        }
+    }
+}
