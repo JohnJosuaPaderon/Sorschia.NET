bf0bdd16d818c3505684a2bfea803abda1524421
john josua paderon <johnjosua.paderon@outlook.com>
2018-01-21 19:11:22 +0800

    Added: IGetAssignmentStatus.cs; IGetAssignmentStatusByKey.cs;

---
+++ MyDayManager/Entity/Process/IGetAssignmentStatus.cs
@@ -0,0 +1,9 @@
+﻿using Sorschia.Processing;
+
+namespace MyDayManager.Entity.Process
+{
+    public interface IGetAssignmentStatus : IProcess<IAssignmentStatus>
+    {
+        short Id { get; set; }
+    }
+}
---
+++ MyDayManager/Entity/Process/IGetAssignmentStatusByKey.cs
@@ -0,0 +1,9 @@
+﻿using Sorschia.Processing;
+
+namespace MyDayManager.Entity.Process
+{
+    public interface IGetAssignmentStatusByKey : IProcess<IAssignmentStatus>
+    {
+        string Key { get; set; }
+    }
+}
