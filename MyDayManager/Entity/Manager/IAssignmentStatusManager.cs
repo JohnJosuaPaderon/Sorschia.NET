920fc0ae441c5963c07119c921a72d99343ec827
john josua paderon <johnjosua.paderon@outlook.com>
2018-01-21 19:11:00 +0800

    Added: IAssignmentStatusManager.cs;

---
+++ MyDayManager/Entity/Manager/IAssignmentStatusManager.cs
@@ -0,0 +1,16 @@
+﻿using Sorschia.Processing;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MyDayManager.Entity.Manager
+{
+    public interface IAssignmentStatusManager
+    {
+        IProcessResult<IAssignmentStatus> Get(short id);
+        Task<IProcessResult<IAssignmentStatus>> GetAsync(short id);
+        Task<IProcessResult<IAssignmentStatus>> GetAsync(short id, CancellationToken cancellationToken);
+        IProcessResult<IAssignmentStatus> GetByKey(string key);
+        Task<IProcessResult<IAssignmentStatus>> GetByKeyAsync(string key);
+        Task<IProcessResult<IAssignmentStatus>> GetByKeyAsync(string key, CancellationToken cancellationToken);
+    }
+}
