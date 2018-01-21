9e4916a7f32fb3123d0097882e8ed57268594126
john josua paderon <johnjosua.paderon@outlook.com>
2018-01-22 01:05:22 +0800

    Added: AssignmentAddedFeed.cs; AssignmentDeletedFeed.cs; AssignmentEnumerableAddedFeed.cs; AssignmentEnumerableDeletedFeed.cs; AssignmentEnumerableUpdatedFeed.cs; AssignmentUpdatedFeed.cs;

---
+++ MyDayManager/Entity/Event/AssignmentDeletedFeed.cs
@@ -0,0 +1,15 @@
+﻿using Sorschia;
+using Sorschia.Events;
+
+namespace MyDayManager.Entity.Event
+{
+    public sealed class AssignmentDeletedFeed : SorschiaEventFeedBase, ISorschiaEventFeed
+    {
+        public AssignmentDeletedFeed(IAssignment assignment)
+        {
+            Assignment = assignment ?? throw SorschiaException.ParameterRequired(nameof(assignment));
+        }
+
+        public IAssignment Assignment { get; }
+    }
+}
---
+++ MyDayManager/Entity/Event/AssignmentUpdatedFeed.cs
@@ -0,0 +1,15 @@
+﻿using Sorschia;
+using Sorschia.Events;
+
+namespace MyDayManager.Entity.Event
+{
+    public sealed class AssignmentUpdatedFeed : SorschiaEventFeedBase, ISorschiaEventFeed
+    {
+        public AssignmentUpdatedFeed(IAssignment assignment)
+        {
+            Assignment = assignment ?? throw SorschiaException.ParameterRequired(nameof(assignment));
+        }
+
+        public IAssignment Assignment { get; }
+    }
+}
