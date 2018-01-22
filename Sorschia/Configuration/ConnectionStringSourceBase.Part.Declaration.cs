2381790979fe4f5ec88fd7a2c12a8d240d9c1287
john josua paderon <johnjosua.paderon@outlook.com>
2018-01-22 10:07:20 +0800

    Added: ConnectionStringSourceBase.Part.Declaration.cs; ConnectionStringSourceBase.Part.Process.cs;
    Updated: ConnectionStringSourceBase.cs;

---
+++ Sorschia/Configuration/ConnectionStringSourceBase.Part.Declaration.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Security;
+
+namespace Sorschia.Configuration
+{
+    partial class ConnectionStringSourceBase
+    {
+        private readonly Dictionary<string, SecureString> _SecureSource;
+        private readonly IConnectionStringSourceLoader _Loader;
+
+        private bool IsLoaded;
+        public bool IsEncrypted { get; set; }
+    }
+}
--- Sorschia/Configuration/ConnectionStringSourceBase.cs
+++ Sorschia/Configuration/ConnectionStringSourceBase.cs
@@ -1,125 +1,15 @@
-﻿using Sorschia.Utilities;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Security;
 
 namespace Sorschia.Configuration
 {
-    public abstract class ConnectionStringSourceBase : IConnectionStringSource
+    public abstract partial class ConnectionStringSourceBase : IConnectionStringSource
     {
         public ConnectionStringSourceBase(IConnectionStringSourceLoader loader)
         {
             IsLoaded = false;
             _SecureSource = new Dictionary<string, SecureString>();
             _Loader = loader;
-        }
-
-        private readonly Dictionary<string, SecureString> _SecureSource;
-        private readonly IConnectionStringSourceLoader _Loader;
-        
-        private bool IsLoaded;
-
-        public bool IsEncrypted { get; set; }
-
-        public string this[string key]
-        {
-            get
-            {
-                ValidateKey(key);
-                TryInitialize();
-                if (_SecureSource.ContainsKey(key))
-                {
-                    return SecureStringConverter.Convert(_SecureSource[key]);
-                }
-                else
-                {
-                    throw NotFound();
-                }
-            }
-            set
-            {
-                ValidateKey(key);
-                ValidateConnectionString(value);
-                TryInitialize();
-                if (_SecureSource.ContainsKey(key))
-                {
-                    _SecureSource[key] = SecureStringConverter.Convert(value);
-                }
-                else
-                {
-                    throw NotFound();
-                }
-            }
-        }
-
-        public void Add(string key, string connectionString)
-        {
-            ValidateKey(key);
-            TryInitialize();
-            if (string.IsNullOrWhiteSpace(key))
-            {
-                throw SorschiaException.ParameterRequired(nameof(key));
-            }
-            else if (_SecureSource.ContainsKey(key))
-            {
-                throw AlreadyExists();
-            }
-            else
-            {
-                _SecureSource.Add(key, SecureStringConverter.Convert(connectionString));
-            }
-        }
-
-        public void Clear()
-        {
-            TryInitialize();
-            _SecureSource.Clear();
-        }
-
-        public void Remove(string key)
-        {
-            ValidateKey(key);
-            TryInitialize();
-            _SecureSource.Remove(key);
-        }
-
-        public void Initialize()
-        {
-            _Loader.Load(this);
-        }
-
-        private SorschiaException NotFound()
-        {
-            return SorschiaException.CollectionItemNotExists("Connection string with the specified key doesn't exists.");
-        }
-
-        private SorschiaException AlreadyExists()
-        {
-            return SorschiaException.CollectionItemDuplication("Connection string with the specified key already exists.");
-        }
-
-        private void TryInitialize()
-        {
-            if (!IsLoaded)
-            {
-                IsLoaded = true;
-                Initialize();
-            }
-        }
-
-        private void ValidateKey(string key)
-        {
-            if (string.IsNullOrWhiteSpace(key))
-            {
-                throw SorschiaException.ParameterRequired(nameof(key));
-            }
-        }
-        
-        private void ValidateConnectionString(string connectionString)
-        {
-            if (string.IsNullOrWhiteSpace(connectionString))
-            {
-                throw SorschiaException.ParameterRequired(nameof(connectionString));
-            }
         }
     }
 }
