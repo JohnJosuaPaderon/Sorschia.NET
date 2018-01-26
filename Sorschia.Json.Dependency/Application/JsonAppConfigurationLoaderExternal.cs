5dddf490827cdd36106791e74c13b1bcce540acf
john josua paderon <johnjosua.paderon@outlook.com>
2018-01-26 10:17:03 +0800

    Added: JsonAppConfigurationLoaderExternal.cs;

---
+++ Sorschia.Json.Dependency/Application/JsonAppConfigurationLoaderExternal.cs
@@ -0,0 +1,13 @@
+﻿using System.Composition;
+
+namespace Sorschia.Application
+{
+    [Export(typeof(IAppConfigurationLoaderExternal))]
+    public sealed class JsonAppConfigurationLoaderExternal : IAppConfigurationLoaderExternal
+    {
+        public IAppConfigurationLoader GetExternal()
+        {
+            return JsonAppConfigurationLoader.Instance;
+        }
+    }
+}
