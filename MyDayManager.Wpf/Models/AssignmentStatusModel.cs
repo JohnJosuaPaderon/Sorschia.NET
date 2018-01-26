fb14dfffe05372d55cfe570ce64426242edb7c31
john josua paderon <johnjosua.paderon@outlook.com>
2018-01-26 09:16:23 +0800

    Added: AssignmentModel.cs; AssignmentStatusModel.cs;

---
+++ MyDayManager.Wpf/Models/AssignmentStatusModel.cs
@@ -0,0 +1,25 @@
+﻿using MyDayManager.Entity;
+using Sorschia.Models;
+
+namespace MyDayManager.Models
+{
+    internal sealed class AssignmentStatusModel : EntityModelBase<IAssignmentStatus, short>
+    {
+        public static AssignmentStatusModel TryInitialize(IAssignmentStatus source)
+        {
+            return source != null ? new AssignmentStatusModel(source) : null;
+        }
+
+        public AssignmentStatusModel(IAssignmentStatus source) : base(source)
+        {
+            Description = source.Description;
+        }
+
+        public string Description { get; }
+
+        public override IAssignmentStatus GetSource()
+        {
+            return base.GetSource();
+        }
+    }
+}
