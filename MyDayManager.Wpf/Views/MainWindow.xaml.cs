f5c210b377ea5a719823cdd691305966bd7593fc
john josua paderon <johnjosua.paderon@outlook.com>
2018-01-26 09:18:22 +0800

    Added: MainView.xaml; MainView.xaml.cs; MainWindow.xaml; MainWindow.xaml.cs;

---
+++ MyDayManager.Wpf/Views/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using MahApps.Metro.Controls;
+
+namespace MyDayManager.Views
+{
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
