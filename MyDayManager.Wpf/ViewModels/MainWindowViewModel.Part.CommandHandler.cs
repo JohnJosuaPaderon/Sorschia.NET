0a60be7ccb4dccf865375412a5bf07e23d156233
john josua paderon <johnjosua.paderon@outlook.com>
2018-01-26 09:17:59 +0800

    Added: MainViewModel.cs; MainWindowViewModel.cs; MainWindowViewModel.Part.CommandHandler.cs; MainWindowViewModel.Part.Declaration.cs; MainWindowVIewModel.Part.EventHandler.cs;

---
+++ MyDayManager.Wpf/ViewModels/MainWindowViewModel.Part.CommandHandler.cs
@@ -0,0 +1,14 @@
+﻿using MyDayManager.Views;
+
+namespace MyDayManager.ViewModels
+{
+    partial class MainWindowViewModel
+    {
+        protected override void Initialize()
+        {
+            _TitleBarTextEvent.Change(VERSION);
+            _AppMessageEvent.Information(VERSION);
+            _RegionManager.RequestNavigate(RegionName, nameof(MainView));
+        }
+    }
+}
---
+++ MyDayManager.Wpf/ViewModels/MainWindowViewModel.Part.EventHandler.cs
@@ -0,0 +1,17 @@
+﻿using Sorschia.Events;
+
+namespace MyDayManager.ViewModels
+{
+    partial class MainWindowViewModel
+    {
+        private void TitleBarTextChanged(string title)
+        {
+            Title = title;
+        }
+
+        private void AppMessageChanged(AppMessage message)
+        {
+            MessageQueue.Enqueue(message.Content);
+        }
+    }
+}
